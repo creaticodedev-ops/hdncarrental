eadcdf6091afb5206ce3a30b6b244c209d9ec0ee
Zakaria Douami <zakariadouami1999@gmail.com>
2026-08-07 15:40:12 +0200

    Add instagram url

--- client/src/constants/brand.js
+++ client/src/constants/brand.js
@@ -1,6 +1,6 @@
 export const BRAND_NAME = 'HDN Car'
 
 /** Official HDN Car Instagram — keep Footer / mobile header in sync */
-export const INSTAGRAM_URL = 'https://www.instagram.com/hdncar/'
+export const INSTAGRAM_URL = 'https://www.instagram.com/haddane_car?igsh=MTY0NzI5dGl0cmQ3OQ=='
 
 export default BRAND_NAME
